bf85e14d4a77a7be8cdca36d21da553c67647f5e
georginapoate <georgina.poting@gmail.com>
2025-09-05 22:08:29 +0300

    added another env var

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 
 // Configure CORS to allow requests from your frontend
 app.use(cors({
-  origin: 'http://127.0.0.1:3000',
+  origin: process.env.FRONTEND_URL || 'http://127.0.0.1:3000',
   credentials: true,
 }));
 
