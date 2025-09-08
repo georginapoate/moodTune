301acec7dc3bfef682a8748ec3bee470156134e6
georginapoate <georgina.poting@gmail.com>
2025-09-08 22:56:16 +0300

    changed order

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,9 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use(cookieParser());
+app.use(express.json({ limit: '50mb' }));
+
 const mongoUrl = process.env.MONGO_URI;
 if (!mongoUrl) {
   throw new Error("❌ FATAL: MONGO_URI is not defined in environment variables.");
@@ -33,7 +36,7 @@ if (!mongoUrl) {
 app.use(session({
   secret: process.env.SESSION_SECRET, // Adaugă o variabilă SESSION_SECRET în .env!
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
   store: MongoStore.create({
     mongoUrl,
     collectionName: 'sessions',
@@ -48,10 +51,7 @@ app.use(session({
   }
 }));
 
-// Standard body and cookie parsers
-app.use(express.json());
-app.use(express.json({ limit: '50mb' }));
-app.use(cookieParser());
+
 
 // --- ROUTES SETUP ---
 const authRoutes = require('./src/routes/authRoutes');
